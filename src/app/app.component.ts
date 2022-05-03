b7656db3b06224563cf61542baef374c6f36d7ac
Vaibhav Talele <v.talele@zensar.com>
2022-05-03 14:51:55 +0530

    ProductPage

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Find great films on your favourite service';
+  title1 = 'Based on 2,396,229 film reviews';
+}
